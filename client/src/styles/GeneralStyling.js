af39d291b69c7c62ddec3024e5c1d7ecd014d980
Nolan Allen <nolan.r.allen@gmail.com>
2019-05-20 12:59:04 -0500

    3rd commit: previous commit accidentally added to master branch, switching to nolan-allen overwrote original file changes

---
+++ client/src/styles/GeneralStyling.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const BackDrop = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #fffbf7;
+`;
+
+export const CenterStage = styled.div`
+  height: 99.8%;
+  width: 99.8%;
+  margin: 0.1%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ client/src/views/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CenterStage } from '../styles/BaseStyling';
+
+export default class HomeView extends React.Component {
+  render() {
+    return (
+      <CenterStage>
+        Everything is going to be ok.
+      </CenterStage>
+    );
+  };
+};
